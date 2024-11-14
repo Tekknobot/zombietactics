d8c18ad916de4b0d394c7fab098ccf1d92d08c78
Tekknobot <thrillatron@hotmail.com>
2024-11-14 16:00:24 +0530

    Create rain randomly.

---
+++ scripts/RainSystem.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Randomly determine whether the node should be visible or not
+	var is_visible = randi() % 2 == 0  # Randomly pick true or false
+	
+	visible = is_visible  # Set visibility based on the random result
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

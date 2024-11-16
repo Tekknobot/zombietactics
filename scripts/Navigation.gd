8ae34f3e79da75e1a2a2855e97d509fbc8cdc1cc
Tekknobot <thrillatron@hotmail.com>
2024-11-16 23:24:04 +0530

    Add Title screen button.

---
+++ scripts/Navigation.gd
@@ -0,0 +1,16 @@
+extends Control
+
+# Load your game scene
+@onready var game_scene_path = "res://assets/scenes/TitleScreen.tscn"  # Adjust the path to your scene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Connect button signals
+	$HBoxContainer/Button.pressed.connect(_on_menu_button_pressed)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _on_menu_button_pressed():
+	get_tree().change_scene_to_file(game_scene_path)  # Use the updated method

57570f66acaa38b6ceac3007455721d200e09728
Tekknobot <thrillatron@hotmail.com>
2024-12-02 11:50:17 +0530

    Refactor Dynamite node.

--- scripts/DynamiteManager.gd
+++ scripts/DynamiteManager.gd
@@ -288,21 +288,14 @@ func _trigger_explosion(last_point: Vector2):
 			
 	# Check for PlayerUnit within explosion radius
 	for player in get_tree().get_nodes_in_group("player_units"):
-		if player.position.distance_to(last_point) <= explosion_radius:
-			if player.player_name == "Yoshida. Boi":
-				pass
-			else:
-				player.get_child(0).play("death")
-				
-			await get_tree().create_timer(0).timeout
-			player.visible = false  # Hide the player unit
-			player.remove_from_group("player_units")  # Remove from the group
-			print("Player Unit removed from explosion")
-			
-			xp_awarded = true
-			
-			var hud_manager = get_parent().get_parent().get_node("HUDManager") 
-			hud_manager.update_hud(player)			
+		if player.position.distance_to(last_point) <= explosion_radius:	
+			player.flash_damage()
+			player.apply_damage(player.attack_damage)
+					
+			xp_awarded = true  # Mark XP as earned for this explosion
+
+			var hud_manager = get_parent().get_node("HUDManager") 
+			hud_manager.update_hud(player)	
 
 	# Check for ZombieUnit within explosion radius
 	for zombie in get_tree().get_nodes_in_group("zombies"):

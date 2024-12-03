04caf44c617829343fb9001bf492f3854423c212
Tekknobot <thrillatron@hotmail.com>
2024-12-04 00:55:01 +0530

    Add mini portrait hud.

--- scripts/Navigation.gd
+++ scripts/Navigation.gd
@@ -6,7 +6,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Connect button signals
-	$HBoxContainer/Button.pressed.connect(_on_menu_button_pressed)
+	$Button.pressed.connect(_on_menu_button_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:

3c1493e18d05c60bff00edb593332c78470a8b88
Tekknobot <thrillatron@hotmail.com>
2024-12-17 14:31:47 +0530

    Update date weather effect particle systems.

--- scripts/ParticleSystem.gd
+++ scripts/ParticleSystem.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
+@export var rain: Node2D
+@export var snow: Node2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Randomly determine whether the node should be visible or not
-	#var is_visible = randi() % 2 == 0  # Randomly pick true or false
-	
-	#visible = is_visible  # Set visibility based on the random result
+	var is_visible = randi() % 2 == 0  # Randomly pick true or false
+	visible = is_visible  # Set visibility based on the random result
+
+	if rain.visible:
+		snow.visible = false
+	if snow.visible:
+		rain.visible = false
 
 	pass
 	
